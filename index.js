df8eefdf66af0f1c1d7422711a3b241cf2d69749
raihanyusri <raihan.yusri@gmail.com>
2023-11-13 14:00:16 +0800

    bug fix

--- index.js
+++ index.js
@@ -8,21 +8,12 @@ const http = require('http').Server(app);
 const cors = require('cors');
 app.use(cors());
 
-let axiosFetch = axios.create();
-axiosFetch.defaults.headers.common["Authorization"] =
-    "Bearer " + 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJET0NUT1IiXSwic3ViIjoiZG9jdG9yQ2FyZGlvbG9neTEiLCJpYXQiOjE2OTk1MDY5NTQsImV4cCI6MTcwMDExMTc1NH0.MkuKdBQtgSq43MTBKs2yr9YirCT-h0Uc7DEHNgTNhXM';
-
-
 // Set up socket.io
 const socketIO = require('socket.io')(http, {
   cors: {
       origin: ["http://localhost:3000", "http://127.0.0.1:5173/"]
   }
 });
-
-// Array of users in chat room
-let users = [];
-
 // On connection
 socketIO.on('connection', (socket) => {
   console.log(`+++ : ${socket.id} user just connected!`);
@@ -30,12 +21,15 @@ socketIO.on('connection', (socket) => {
   // On message sent
   socket.on('message', (data) => {
     console.log("sent by: " + socket.id)
+    postMessage(data.conversationId, data.senderId, data.content, data.token)
     socketIO.emit('messageResponse', data);
-    postMessage(data.conversationId, data.senderId, data.content)
   });
 
-  const postMessage = async (conversationId ,senderId, content) => {
+  const postMessage = async (conversationId ,senderId, content, token) => {
     try {
+      let axiosFetch = axios.create();
+      axiosFetch.defaults.headers.common["Authorization"] =
+        "Bearer " + token;
        const response = await axiosFetch.post(`http://localhost:8080/conversation/addChatMessage?conversationId=${conversationId}&senderId=${senderId}&content=${content}`)
        console.log(response)
     } catch (e) {
@@ -52,7 +46,7 @@ socketIO.on('connection', (socket) => {
   // On user disconnect
   socket.on('disconnect', () => {
     console.log(`--- : ${socket.id} user disconnected`);
-    socketIO.emit('newUserResponse', users);
+    // socketIO.emit('newUserResponse', users);
     socket.disconnect();
   });
 });
